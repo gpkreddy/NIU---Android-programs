fc22f0768a9ee903054caaef6397e41b0ea55f7e
Praveen Gajjala <gpkreddy29@gmail.com>
2012-02-08 20:45:27 -0600

    testing

---
+++ assgn1/src/edu/cs/niu/assg1/Assgn1Activity.java
@@ -0,0 +1,13 @@
+package edu.cs.niu.assg1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Assgn1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
