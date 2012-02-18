1859d50a92e77af156a1690f4763baa990aad516
gpkreddy <gpkreddy29@gmail.com>
2012-02-17 20:51:33 -0600

    Initial Commit

---
+++ Assg2 - Mortgage Calculator/src/edu/niu/cs/z1631118/Assg2MortgageCalculatorActivity.java
@@ -0,0 +1,13 @@
+package edu.niu.cs.z1631118;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Assg2MortgageCalculatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
